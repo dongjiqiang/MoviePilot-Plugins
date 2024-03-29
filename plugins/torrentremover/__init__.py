2a8f7ff7a481dbd3a451a906312f8a0e7f304e14
董继强 <834143263@qq.com>
2024-03-29 12:14:12 +0800

    fix: 修改配置

--- plugins/torrentremover/__init__.py
+++ plugins/torrentremover/__init__.py
@@ -33,7 +33,7 @@ class TorrentRemoverGai(_PluginBase):
     # 作者主页
     author_url = "https://github.com/jxxghp"
     # 插件配置项ID前缀
-    plugin_config_prefix = "torrentremovergai_"
+    plugin_config_prefix = "TorrentRemoverGai_"
     # 加载顺序
     plugin_order = 8
     # 可使用的用户级别
