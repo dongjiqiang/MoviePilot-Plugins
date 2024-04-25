017aa81c33b221c32151c8750c49617acef33130
董继强 <834143263@qq.com>
2024-04-25 22:40:54 +0800

    feat: 修改消息样式

--- plugins/dingtalkbotmsg/__init__.py
+++ plugins/dingtalkbotmsg/__init__.py
@@ -15,7 +15,7 @@ class DingTalkBotMsg(_PluginBase):
     # 插件图标
     plugin_icon = "https://mp-baa3849c-ebde-4ec9-9161-f833c19d9a61.cdn.bspapp.com/dingtalk.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "dongjiqiang"
     # 作者主页
@@ -179,7 +179,7 @@ class DingTalkBotMsg(_PluginBase):
                     "msgtype": "markdown",
                     "markdown": {
                         "title": title,
-                        "text": title + "\n" + text,
+                        "text": "#### **" + title + "** \n" + text.replace('\n', ' \n > ###### ')
                     }
                 }
             else:
@@ -187,7 +187,7 @@ class DingTalkBotMsg(_PluginBase):
                     "msgtype": "markdown",
                     "markdown": {
                         "title": title,
-                        "text": title + "\n" + text + "\n" + image,
+                        "text": "#### **" + title + "** \n" + text.replace('\n', ' \n > ###### ') + "\n ![image](" + image + ")"
                     }
                 }
 
